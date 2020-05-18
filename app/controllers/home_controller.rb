475aa692bb41d7a5a686791af70c37cd73b8e485
debicky <arturd444@gmail.com>
2020-05-18 23:35:32 +0200

    a bit impoved layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index]
+
+
   def index
+    @courses = Course.all.limit(3)
+    @recent_courses = Course.limit(3).order("created_at DESC")
   end
 end
