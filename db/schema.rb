fb2ee0d81c8d851c5e3bc3ce83bb90bcdece6757
debicky <arturd444@gmail.com>
2020-05-26 22:15:48 +0200

    added counter cache for ennrolments count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_192020) do
+ActiveRecord::Schema.define(version: 2020_05_26_200519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_192020) do
     t.string "language", default: "English", null: false
     t.integer "price", default: 0, null: false
     t.float "average_rating", default: 0.0, null: false
+    t.integer "enrollments_count", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
