24b7bca678d1aca367c6bd3f0a02eea105db86cf
debicky <arturd444@gmail.com>
2020-05-28 14:01:18 +0200

    a bit styyling for index course page

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -30,7 +30,7 @@ class CoursePolicy < ApplicationPolicy
   end
   
   def destroy?
-    @record.user == @user || @user.has_role?(:admin)
+    @record.user == @user && @record.enrollments.none? || @user.has_role?(:admin) && @record.enrollments.none?
   end
 
   def admin?
