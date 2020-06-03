5150006cd7ad7c6347a6a56d58a43ded42b1ba3c
debicky <arturd444@gmail.com>
2020-06-03 15:46:07 +0200

    added videos, videos validation and video thumbnail

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,12 +2,20 @@ class Lesson < ApplicationRecord
   belongs_to :course, counter_cache: true
   validates :title, :content, :course, presence: true
   validates :title, length: { maximum: 100}
+  validates_uniqueness_of :title, scope: :course_id
 
   #Course.find_each { |course| Course.reset_counters(course.id, :courses)}
 
   has_many :user_lessons, dependent: :destroy
 
   has_rich_text :content
+  has_one_attached :video
+  has_one_attached :video_thumbnail
+  validates :video, content_type: ['video/mp4'], 
+  size: { less_than: 50.megabytes , message: 'Maximum size is 50 megabytes.' }
+  validates :video_thumbnail, content_type: ['image/png', 'image/jpg', 'image/jpeg'], 
+  size: { less_than: 500.kilobytes , message: 'Maximum size is 500 kilobytes.' }
+
 
 
   extend FriendlyId
